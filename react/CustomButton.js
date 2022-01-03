08d046df76faf25e9155fa576a06268db48e9ec6
akshay-hegde96 <akshay.sh@echidnainc.com>
2022-01-03 14:28:09 +0530

    initial-commit

---
+++ react/CustomButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from 'vtex.styleguide'
+// import styles from './ViewDetailsButton.css'
+import { useCssHandles } from 'vtex.css-handles'
+
+const CSS_HANDLES = ['buttonContainer', 'buttonLabel']
+
+
+
+function CustomButton(props) {
+
+  const handles = useCssHandles(CSS_HANDLES)
+return (
+
+   <div className={handles.buttonContainer}>
+     <Button {...props} ><span className={handles.buttonLabel}>{props.buttonLabel}</span></Button>
+   </div>
+
+)
+}
+
+export default CustomButton
